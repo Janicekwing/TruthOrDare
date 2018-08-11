f25db9f333faf632ec05057452ca0ac944bbd7f6
Janice He <janicekwhe@gmail.com>
2018-08-11 15:39:34 -0400

    "Use this finished Truth or Dare app as an example to teach iOS Development"

--- TruthOrDare/ViewController.swift
+++ TruthOrDare/ViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var truthButton: UIButton!
+    @IBOutlet weak var dareButton: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var backButton: UIButton!
+    
+    
+    
+    var truthArray: [String] = ["When did you have your first kiss?", "What is your biggest regret?", "What is your biggest fear?", "What is your last text message?"]
+    
+    lazy var truthCount: Int = truthArray.count
+    
+    var dareArray: [String] = ["Give someone your phone and let them send one text to anyone in your contacts", "Text someone you are close to and tell them you no longer want to talk to them", "Post something good about Trump on your Facebook status"]
+    
+    lazy var dareCount: Int = dareArray.count
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        backButton.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +36,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func truthButton(_ sender: UIButton) {
+        truthButton.isHidden = true
+        dareButton.isHidden = true
+        let randomTruthIndex: Int = Int(arc4random_uniform(UInt32(truthCount)))
+        resultLabel.text = truthArray[randomTruthIndex]
+        resultLabel.isHidden = false
+        backButton.isHidden = false
+        
+    }
+    
+    @IBAction func dareButton(_ sender: UIButton) {
+        truthButton.isHidden = true
+        dareButton.isHidden = true
+        let randomDareIndex: Int = Int(arc4random_uniform(UInt32(dareCount)))
+        resultLabel.text = dareArray[randomDareIndex]
+        resultLabel.isHidden = false
+        backButton.isHidden = false
+    }
+    
+    @IBAction func backButton(_ sender: UIButton) {
+        truthButton.isHidden = false
+        dareButton.isHidden = false
+        resultLabel.isHidden = true
+        backButton.isHidden = true
+    }
+    
+    
 }
 
